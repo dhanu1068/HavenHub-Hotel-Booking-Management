693937a8b8e62475fd32623e9c3788f1662c681c
dhanu1068 <dhanu1068@gmail.com>
2025-09-20 04:02:39 +0530

    Your commit message here

--- src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
+++ src/main/java/com/dailycodework/lakesidehotel/security/CorsConfig.java
@@ -28,7 +28,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://havenhub-app.vercel.app");
         config.setAllowedHeaders(Arrays.asList(
                 HttpHeaders.AUTHORIZATION,
                 HttpHeaders.CONTENT_TYPE,
